248f420c9eb50ea2c05d2580b3673814244bbeaa
陈嘉劲 <450883304@qq.com>
2020-03-19 22:02:55 +0800

    优化了注释的视觉效果

--- src/main/java/chenjiajin/rocketmqdemo/jms/PayConsumer.java
+++ src/main/java/chenjiajin/rocketmqdemo/jms/PayConsumer.java
@@ -17,17 +17,22 @@ public class PayConsumer {
 
     private DefaultMQPushConsumer consumer;
 
-    private String consumerGroup = "pay_consumer_group"; //所属的组 干嘛用我也不知道
+    //所属的组 干嘛用我也不知道
+    private String consumerGroup = "pay_consumer_group";
 
     public PayConsumer() throws MQClientException {
-        consumer = new DefaultMQPushConsumer(consumerGroup); //配置消费者组
-        consumer.setNamesrvAddr(jmsConfig.NAME_SERVER_ADDR);//配置消费的地址
-
-        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);//设置消费方式，从最后一个开始消费
-        consumer.subscribe(jmsConfig.TOPIC, "*");//定义消费的主题
+        //配置消费者组
+        consumer = new DefaultMQPushConsumer(consumerGroup);
+        //配置消费的地址
+        consumer.setNamesrvAddr(jmsConfig.NAME_SERVER_ADDR);
+        //设置消费方式，从最后一个开始消费
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        //定义消费的主题
+        consumer.subscribe(jmsConfig.TOPIC, "*");
         consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
             MessageExt msg = msgs.get(0);
-            int times = msg.getReconsumeTimes();   //获取消息重复消费次数
+            //获取消息重复消费次数
+            int times = msg.getReconsumeTimes();
             try {
                 System.out.printf("%s Receive New Messages: %s %n",
                         Thread.currentThread().getName(), new String(msgs.get(0).getBody()));
@@ -36,7 +41,8 @@ public class PayConsumer {
                 String tags = msg.getTags();
                 String keys = msg.getKeys();
                 System.out.println("topic=" + topic + ", tags=" + tags + ", keys=" + keys + ", msg=" + body);
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS; //消息发送成功
+                //消息发送成功
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
             } catch (UnsupportedEncodingException e) {
                 System.out.println("消费异常");
                 //如果重试2次不成功，则记录，人工介入
@@ -46,7 +52,8 @@ public class PayConsumer {
                     //告诉broker，消息成功 等于我不发了 浪费资源艾玛
                     return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                 }
-                return ConsumeConcurrentlyStatus.RECONSUME_LATER; //消息发送失败
+                //消息发送失败
+                return ConsumeConcurrentlyStatus.RECONSUME_LATER;
             }
         });
         consumer.start();
--- src/main/java/chenjiajin/rocketmqdemo/jms/jmsConfig.java
+++ src/main/java/chenjiajin/rocketmqdemo/jms/jmsConfig.java
@@ -2,8 +2,10 @@ package chenjiajin.rocketmqdemo.jms;
 
 public class jmsConfig {
 
-    public static final String NAME_SERVER_ADDR = "127.0.0.1:9876";  //这里写mq的地址，我写线下的，可以换成线上
+    //这里写mq的地址，我写线下的，可以换成线上
+    public static final String NAME_SERVER_ADDR = "127.0.0.1:9876";
 
+    //设置消费主题
     public static final String TOPIC = "xdclass_pay_test_topic";
 
 }
