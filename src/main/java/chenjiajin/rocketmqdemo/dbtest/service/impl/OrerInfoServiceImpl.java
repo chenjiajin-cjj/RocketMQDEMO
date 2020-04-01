cf559af6463145c752f2812f50972a56dee9c174
陈嘉劲 <450883304@qq.com>
2020-04-01 14:31:08 +0800

    随便更新点没用的东西1

--- src/main/java/chenjiajin/rocketmqdemo/dbtest/service/impl/OrerInfoServiceImpl.java
+++ src/main/java/chenjiajin/rocketmqdemo/dbtest/service/impl/OrerInfoServiceImpl.java
@@ -28,39 +28,39 @@ import java.util.UUID;
  */
 @Service
 public class OrerInfoServiceImpl extends ServiceImpl<OrerInfoMapper, OrerInfo> implements IOrerInfoService {
-
     public static final String TOPIC = "order_db_topic";
-
     //注入mq的配置
     @Autowired
     private PayProducer payProducer;
-
     @Override
     public Object addToOrder(String json) {
-
-        OrerInfo orerInfo = JSONObject.parseObject(json, OrerInfo.class);
         //设置唯一的uuid
         String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-
         //往队列里赛消息
         Message message = new Message(TOPIC, "", uuid, json.getBytes());
-
         //设置发送的延迟时间 1是1s
         message.setDelayTimeLevel(0);
         int a = 0;
         //异步发送
         try {
-            payProducer.getProducer().send(message);
+            payProducer.getProducer().send(message, new SendCallback() {
+                @Override
+                public void onSuccess(SendResult sendResult) {
+                    System.out.printf("订单异步发送结果=%s, msg=%s", sendResult.getSendStatus(), sendResult.toString());
+                }
+
+                @Override
+                public void onException(Throwable throwable) {
+                    System.out.println("订单异步发送失败了艾玛");
+                }
+            });
         } catch (MQClientException e) {
             e.printStackTrace();
         } catch (RemotingException e) {
             e.printStackTrace();
-        } catch (MQBrokerException e) {
-            e.printStackTrace();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return "ppppp";
-
+        return "创建成功";
     }
 }
--- src/main/java/chenjiajin/rocketmqdemo/dbtest/web/OrerInfoController.java
+++ src/main/java/chenjiajin/rocketmqdemo/dbtest/web/OrerInfoController.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
  *  前端控制器
  * </p>
- *
  * @author chenjiajin@Jowim.com
  * @since 2020-03-21
  */
-@Controller
+@RestController
 @RequestMapping("/jowim/orerInfo")
 public class OrerInfoController {
 
