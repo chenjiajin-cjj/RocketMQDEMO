98d7ded5076f8f77ef44fdc9f66cbe690906fdef
陈嘉劲 <450883304@qq.com>
2020-04-01 16:33:32 +0800

    添加测试版本的注解

--- src/main/java/chenjiajin/rocketmqdemo/controller/RedisController.java
+++ src/main/java/chenjiajin/rocketmqdemo/controller/RedisController.java
@@ -2,7 +2,11 @@ package chenjiajin.rocketmqdemo.controller;
 
 import chenjiajin.rocketmqdemo.dbtest.entity.OrerInfo;
 import chenjiajin.rocketmqdemo.redisall.service.IRedisCache;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/redis")
+//@Component
+//@RunWith(SpringRunner.class) //非mvc模式下使用autowired注入不报空的注解 2个
+//@SpringBootTest
 public class RedisController {
 
     @Autowired
